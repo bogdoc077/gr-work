3b14b02e109b1b7a299c99465c3762dbf5af7c74
Bohdan Hnatiuk <dmitrov.977@yandex.ru>
2020-06-29 17:06:52 +0300

    Готовая верстка страниц

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+
+
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: {
+    index: './src/index.js',
+    about: './src/about/about.js',
+    analytics: './src/analytics/analytics.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[chunkhash].js'
@@ -19,19 +28,67 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      }
+        use:  [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              publicPath: 'img',
+              outputPath: 'img',
+              useRelativePath: true,
+              esModule: false,
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+            }
+          }
+        ]
+      },
+    {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/fonts/[name].[ext]'
+    }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ // 
+    new MiniCssExtractPlugin({
       filename: 'style.[contenthash].css',
+    }),
+    new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorPluginOptions: {
+                preset: ['default'],
+        },
+        canPrint: true
     }),
     new HtmlWebpackPlugin({
       inject: false,
       template: './src/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
+      chunks: ['index']
     }),
-    new WebpackMd5Hash()
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/about.html',
+      filename: 'about.html',
+      chunks: ['about']
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/analytics.html',
+      filename: 'analytics.html',
+      chunks: ['analytics']
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
   ]
 };
